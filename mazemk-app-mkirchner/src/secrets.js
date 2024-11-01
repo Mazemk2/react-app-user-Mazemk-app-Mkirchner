29367365d52de4c00b66a95684888984fedb24e0
Mazemk2 <Mazemk2@googlemail.com>
2024-11-01 07:55:52 +0000

    new configuration

--- mazemk-app-mkirchner/src/secrets.js
+++ mazemk-app-mkirchner/src/secrets.js
@@ -1,1 +1,14 @@
-// i'm not sure yet
+export const db_server = 'localhost:3306';  // Ersetze durch deinen DB-Host
+export const db_username = 'root';  // Ersetze durch deinen DB-Benutzernamen
+export const db_password = 'Root';  // Ersetze durch dein DB-Passwort
+export const db_database = 'project_db';  // Ersetze durch deinen DB-Namen
+export const jwt_secret_key = 'mein-total-geheimer-schluessel';  // Dein geheimer Schlüssel
+
+const secrets = {
+    db_server: 'localhost',
+    db_username: 'root',
+    db_password: 'Root',
+    db_database: 'project_db',
+    jwt_secret_key: '...sdf'
+}
+export default secrets;
